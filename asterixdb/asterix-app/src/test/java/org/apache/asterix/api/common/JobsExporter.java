69f3f6e910ce6afd65f11282b4eab0bd06e7fe15
Krishnateja Avvari <avvarikrish@gmail.com>
2020-11-17 11:14:31 -0800

    Job Exporter: Running jobs bug

--- asterixdb/asterix-app/src/test/java/org/apache/asterix/api/common/JobsExporter.java
+++ asterixdb/asterix-app/src/test/java/org/apache/asterix/api/common/JobsExporter.java
@@ -19,7 +19,7 @@ public class JobsExporter extends Collector {
     public List<MetricFamilySamples> collect() {
         List<MetricFamilySamples> mfs = new ArrayList<MetricFamilySamples>();
         GaugeMetricFamily jobsCount = new GaugeMetricFamily("asterix_job_count", "current count of jobs", Arrays.asList("labelname"));
-        jobsCount.addMetric(Arrays.asList("running_jobs"), jobManager.getPendingJobs().size());
+        jobsCount.addMetric(Arrays.asList("running_jobs"), jobManager.getRunningJobs().size());
         jobsCount.addMetric(Arrays.asList("archived_jobs"), jobManager.getArchivedJobs().size());
         jobsCount.addMetric(Arrays.asList("pending_jobs"), jobManager.getPendingJobs().size());
         mfs.add(jobsCount);
